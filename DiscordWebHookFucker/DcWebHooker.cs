de03c94790aa303c454931534817488d3b9968d6
BlumeCoding <33968913+BlumeCoding@users.noreply.github.com>
2021-05-26 21:22:48 +0200

    Add files via upload

---
+++ DiscordWebHookFucker/DcWebHooker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+public class DcWebHooker : IDisposable
+{
+    private readonly WebClient dWebClient;
+    private static NameValueCollection discordValues = new NameValueCollection();
+    public string WebHook { get; set; }
+    public string UserName { get; set; }
+    public string ProfilePicture { get; set; }
+    private HttpClient _Client;
+
+    private string _URL;
+
+
+
+    public DcWebHooker()
+    {
+        dWebClient = new WebClient();
+
+    }
+
+
+    public void SendMessage(string msgSend)
+    {
+        discordValues.Add("username", UserName);
+        discordValues.Add("avatar_url", ProfilePicture);
+        discordValues.Add("content", msgSend);
+        Thread.Sleep(9000);
+        dWebClient.UploadValues(WebHook, discordValues);
+    }
+
+    public void Dispose()
+    {
+        dWebClient.Dispose();
+    }
+}
---
+++ DiscordWebHookFucker/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiscordWebHookFucker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (DcWebHooker dcWeb = new DcWebHooker())
+            {
+                {
+                    dcWeb.UserName = textBox3.Text;
+                    timer1.Start();
+
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (DcWebHooker dcWeb = new DcWebHooker())
+            {
+                {
+                    timer1.Interval = (int)this.numericUpDown1.Value;
+                    dcWeb.WebHook = textBox2.Text;
+                    dcWeb.ProfilePicture = "https://memegenerator.net/img/instances/29984422.jpg";
+                   
+                    dcWeb.SendMessage(textBox1.Text);
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           // timer1.Interval = (int)this.numericUpDown1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+    }
